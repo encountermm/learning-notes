3e0bc6c67d7f7829b1c71331b89b71a3f8db5fde
encountermm <1120010842@qq.com>
2019-11-04 23:51:09 +0800

    代理proxy

--- javascript/书籍笔记/javascript忍者秘籍/test.js
+++ javascript/书籍笔记/javascript忍者秘籍/test.js
@@ -1,9 +1,19 @@
-const promise = new Promise((resolve, reject) => {
-	//如果在处理promise时出现未处理的异常，则会被隐式地拒绝
-	undeclaredVariable++
-})
-
-// 如果发生了异常，则第二个回调函数error将被调用
-promise
-	.then(() => fail("Happy path, won't be called!"))
-	.catch(error => pass('Third promise was also rejected'))
+const shogun = {
+	name: 'Yoshiaki',
+	clan: 'Ashikaga',
+	get fullTitle() {
+		return this.name + ' ' + this.clan
+	}, //在对象字面量上定义属性fullTitle的getter方法，该方法将name与clan两个属性值拼接在一起
+	set fullTitle(value) {
+		const segments = value.split(' ')
+		this.name = segments[0]
+		this.clan = segments[1]
+	}
+} //在对象字面量上定义属性fullTitle的setter方法，该方法将传入的参数值通过空格分隔开，并分别更新标准属性name与clan的值
+assert(shogun.name === 'Yoshiaki', 'Our shogun Yoshiaki')
+assert(shogun.clan === 'Ashikaga', 'Of clan Ashikaga')
+assert(shogun.fullTitle === 'Yoshiaki Ashikaga', 'The full name is now Yoshiaki Ashikaga') // name与clan属性均是普通属性，具有直接属性值。而访问fullTitle属性的值时将调用对应的get方法计算属性值
+shogun.fullTitle = 'Ieyasu Tokugawa'
+assert(shogun.name === 'Ieyasu', 'Our shogun Ieyasu')
+assert(shogun.clan === 'Tokugawa', 'Of clan Tokugawa')
+assert(shogun.fullTitle === 'Ieyasu Tokugawa', 'The full name is now Ieyasu Tokugawa') //对fullTitle属性赋值时将调用对应的set方法，该方法将计算后分别赋值给name与clan属性
