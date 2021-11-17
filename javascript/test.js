325583b75838e3ff43ed2ea837f9b82054ba3486
encountermm <1120010842@qq.com>
2021-11-17 16:53:23 +0800

    常用函数

--- javascript/test.js
+++ javascript/test.js
@@ -1,48 +1,23 @@
-const isComplexDataType = (obj) => {
-  return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
-}
+function deepClone(obj, cache = new WeakMap()) {
+  if (typeof obj !== 'object') return obj // 普通类型，直接返回
+  if (obj === null) return obj
+  if (cache.get(obj)) return cache.get(obj) // 防止循环引用，程序进入死循环
+  if (obj instanceof Date) return new Date(obj)
+  if (obj instanceof RegExp) return new RegExp(obj)
 
-const deepClone = function (obj, hash = new WeakMap()) {
-  if (obj.constructor === Date) return new Date(obj) // 日期对象直接返回一个新的日期对象
-  if (obj.constructor === RegExp) return new RegExp(obj) //正则对象直接返回一个新的正则对象
-  //如果循环引用了就用 weakMap 来解决
-  if (hash.has(obj)) return hash.get(obj)
-  let allDesc = Object.getOwnPropertyDescriptors(obj)
-  // 遍历传入参数所有键的特性
-  let cloneObj = Object.create(Object.getPrototypeOf(obj), allDesc)
-  // 继承原型链
-  hash.set(obj, cloneObj)
-  for (let key of Reflect.ownKeys(obj)) {
-    cloneObj[key] =
-      isComplexDataType(obj[key]) && typeof obj[key] !== 'function'
-        ? deepClone(obj[key], hash)
-        : obj[key]
+  // 找到所属原型上的constructor，所属原型上的constructor指向当前对象的构造函数
+  let cloneObj = new obj.constructor()
+  cache.set(obj, cloneObj) // 缓存拷贝的对象，用于处理循环引用的情况
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      cloneObj[key] = deepClone(obj[key], cache) // 递归拷贝
+    }
   }
   return cloneObj
 }
-// 下面是验证代码
-let obj = {
-  num: 0,
-  str: '',
-  boolean: true,
-  unf: undefined,
-  nul: null,
-  obj: { name: '我是一个对象', id: 1 },
-  arr: [0, 1, 2],
-  func: function () {
-    console.log('我是一个函数')
-  },
-  date: new Date(0),
-  reg: new RegExp('/我是一个正则/ig'),
-  [Symbol('1')]: 1
-}
-Object.defineProperty(obj, 'innumerable', {
-  enumerable: false,
-  value: '不可枚举属性'
-})
-obj = Object.create(obj, Object.getOwnPropertyDescriptors(obj))
-obj.loop = obj // 设置loop成循环引用的属性
-let cloneObj = deepClone(obj)
-cloneObj.arr.push(4)
-console.log('obj', obj)
-console.log('cloneObj', cloneObj)
+
+// 测试
+const obj = { name: 'Jack', address: { x: 100, y: 200 } }
+obj.a = obj // 循环引用
+const newObj = deepClone(obj)
+console.log(newObj.address === obj.address) // false
